ea85c6dcc9e2d2360021bb33a94afa979a89d33b
rafonersisyan <rafonersisyan01@gmail.com>
2022-04-15 14:11:04 +0400

    done

---
+++ src/components/Cart.js
@@ -0,0 +1,13 @@
+import React from "react";
+import NavigationBar from "./NavigationBar/NavigationBar";
+import Footer from "./Footer/Footer";
+
+export default function Cart() {
+  return (
+    <>
+      <NavigationBar />
+      <div>This is cart</div>
+      <Footer />
+    </>
+  );
+}
--- src/data/SiderBarData.js
+++ src/data/SiderBarData.js
@@ -3,21 +3,40 @@ import MenuIcon from '@material-ui/icons/Menu';
 
 export const SidebarData = [
     {
-        title: 'Գլխավոր էջ',
-        path: '/',
+        title: 'Pizza',
+        path: '/pizza',
         icon: <MenuIcon/>,
         cName: 'nav-text'
     },
     {
-        title: 'Աղցաններ',
+        title: 'Burgers',
+        path: '/burger',
+        icon: <MenuIcon/>,
+        cName: 'nav-text'
+    },
+    {
+        title: 'BBQ',
+        path: '/bbq',
+        icon: <MenuIcon/>,
+        cName: 'nav-text'
+    },
+    {
+        title: 'Salads',
         path: '/salad',
         icon: <MenuIcon/>,
         cName: 'nav-text'
     },
     {
-        title: 'Ապուրներ',
-        path: '/soup',
+        title: 'Drinks',
+        path: '/drink',
+        icon: <MenuIcon/>,
+        cName: 'nav-text'
+    },
+    {
+        title: 'Sweets',
+        path: '/sweet',
         icon: <MenuIcon/>,
         cName: 'nav-text'
     }
+
 ];
