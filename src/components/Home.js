1d68bb851f35e2e4da9e8dabe117a651068bd18b
rafonersisyan <rafonersisyan01@gmail.com>
2022-04-20 15:28:13 +0400

    product card details and functionality

--- src/components/Home.js
+++ src/components/Home.js
@@ -5,25 +5,42 @@ import { ProductContext } from "../contexts/ProductContext";
 import ProductCard from "./ProductCard";
 import homepagePhoto from "../homepagePhoto.jpg";
 import { SearchInputValueContext } from "../contexts/SearchInputValueContext";
+import { makeStyles } from "@material-ui/core";
+
+const useStyles = makeStyles({
+  wrapper: {
+    width: '100%',
+    display: 'flex',
+    justifyContent: 'center',
+    flexWrap: 'wrap',
+  }
+})
 
 export default function Home() {
   const productList = useContext(ProductContext);
   const searchInputValue = useContext(SearchInputValueContext);
+  const classes = useStyles()
+
   return (
     <>
       <NavigationBar />
       <img src={homepagePhoto} style={{ width: "100%" }} />
-      {productList.map((product) => {
-        let value = searchInputValue[0].toLowerCase()
-        
-        if(value){
-          if(value === product.type || value === product.name.toLowerCase()){
-            return <ProductCard key={product.id} product={product} />;
+      <div className={classes.wrapper}>
+        {productList.map((product) => {
+          let value = searchInputValue[0].toLowerCase();
+
+          if (value) {
+            if (
+              value === product.type ||
+              value === product.name.toLowerCase()
+            ) {
+              return <ProductCard key={product.id} product={product} />;
+            }
+            return null;
           }
-          return null
-        }
-        return <ProductCard key={product.id} product={product} />;
-      })}
+          return <ProductCard key={product.id} product={product} />;
+        })}
+      </div>
       <Footer />
     </>
   );
